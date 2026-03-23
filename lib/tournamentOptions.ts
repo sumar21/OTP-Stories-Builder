30d6c8a2181ce558ddac099132b6423e58476bb6
Lautaro Loggia <lauloggia@Mac-de-Lau.local>
2026-03-23 12:13:21 -0300

    feat: Ajustar categorías en Caballeros (C4/C5, C5/C6, C6/C7, C8/C9) y remover D6/D7/D8 en Damas

--- lib/tournamentOptions.ts
+++ lib/tournamentOptions.ts
@@ -8,14 +8,14 @@ const CATEGORY_GROUP_LABELS: Record<Gender, string> = {
 };
 
 export const CATEGORY_OPTIONS_BY_GENDER: Record<Gender, readonly string[]> = {
-  Masculino: ["C3", "C3/C4", "C4", "C5", "C6", "C7", "C8", "C7/C8", "C9"],
-  Femenino: ["D3", "D4", "D5", "D6", "D7", "D8", "D3/D4", "D4/D5", "D5/D6", "D6/D7", "D7/D8", "D6/D7/D8"],
+  Masculino: ["C3", "C4", "C5", "C6", "C7", "C8", "C9", "C3/C4", "C4/C5", "C5/C6", "C6/C7", "C7/C8", "C8/C9"],
+  Femenino: ["D3", "D4", "D5", "D6", "D7", "D8", "D3/D4", "D4/D5", "D5/D6", "D6/D7", "D7/D8"],
   Mixto: ["Suma 7", "Suma 8", "Suma 9", "Suma 10", "Suma 11", "Suma 12", "Suma 13", "Suma 14", "Suma 15", "Suma 16"],
 };
 
 export const LOOSE_PLAYER_CATEGORY_OPTIONS_BY_GENDER: Record<Gender, readonly string[]> = {
-  Masculino: ["C3", "C4", "C5", "C6", "C7", "C8", "C3/C4", "C5/C6", "C6/C7", "C7/C8"],
-  Femenino: ["D3", "D4", "D5", "D6", "D7", "D8", "D3/D4", "D4/D5", "D5/D6", "D6/D7", "D7/D8", "D6/D7/D8"],
+  Masculino: ["C3", "C4", "C5", "C6", "C7", "C8", "C9", "C3/C4", "C4/C5", "C5/C6", "C6/C7", "C7/C8", "C8/C9"],
+  Femenino: ["D3", "D4", "D5", "D6", "D7", "D8", "D3/D4", "D4/D5", "D5/D6", "D6/D7", "D7/D8"],
   Mixto: ["Suma 7", "Suma 8", "Suma 9", "Suma 10", "Suma 11", "Suma 12", "Suma 13", "Suma 14", "Suma 15", "Suma 16"],
 };
 
